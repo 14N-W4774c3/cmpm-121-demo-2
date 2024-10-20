0fbb6d0f205e1edb6026d8d8d95b5610f4a463de
14N-W4774c3 <iawallac@ucsc.edu>
2024-10-20 06:43:33 -0700

    Added cursor and code to draw lines.

--- src/main.ts
+++ src/main.ts
@@ -14,18 +14,27 @@ stickerCanvas.width = 256;
 stickerCanvas.height = 256;
 document.body.appendChild(stickerCanvas);
 const ctx = stickerCanvas.getContext("2d");
+const cursor = {x: 0, y: 0};
 
 if (ctx){
     let drawing: boolean = false;
     stickerCanvas.addEventListener("mousedown", (e) => {
         drawing = true;
         console.log("mousedown", e);
-        ctx.beginPath();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+        
     });
 
     stickerCanvas.addEventListener("mousemove", (e) => {
         if (drawing) {
             console.log("mousemove", e);
+            ctx.beginPath();
+            ctx.moveTo(cursor.x, cursor.y);
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.stroke();
+            cursor.x = e.offsetX;
+            cursor.y = e.offsetY;
             }
     });
 
