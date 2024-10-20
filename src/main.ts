548cf03fb505ff06ca2f20638b3fd04b255506ce
14N-W4774c3 <iawallac@ucsc.edu>
2024-10-20 06:22:52 -0700

    Added canvas context.  Apparently that's needed for drawing.  Also added requirement for said context before doing anything mouse-related, because it's needed for drawing, and error code for if it's not present.

--- src/main.ts
+++ src/main.ts
@@ -13,21 +13,27 @@ const stickerCanvas = document.createElement("canvas");
 stickerCanvas.width = 256;
 stickerCanvas.height = 256;
 document.body.appendChild(stickerCanvas);
+const ctx = stickerCanvas.getContext("2d");
 
-let drawing: boolean = false;
-stickerCanvas.addEventListener("mousedown", (e) => {
-    drawing = true;
-    console.log("mousedown", e);
+if (ctx){
+    let drawing: boolean = false;
+    stickerCanvas.addEventListener("mousedown", (e) => {
+        drawing = true;
+        console.log("mousedown", e);
+        ctx.beginPath();
     });
 
-stickerCanvas.addEventListener("mousemove", (e) => {
-    if (drawing) {
-        console.log("mousemove", e);
-        }
+    stickerCanvas.addEventListener("mousemove", (e) => {
+        if (drawing) {
+            console.log("mousemove", e);
+            }
     });
 
-stickerCanvas.addEventListener("mouseup", (e) => {
-    drawing = false;
-    console.log("mouseup", e);
+    stickerCanvas.addEventListener("mouseup", (e) => {
+        drawing = false;
+        console.log("mouseup", e);
     });
-
+}
+else {
+    console.error("Could not get 2D context from canvas");
+}
