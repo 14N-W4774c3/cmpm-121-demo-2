e67167c100b5a52af211a93abc68e58ee572ac8a
14N-W4774c3 <iawallac@ucsc.edu>
2024-10-22 05:02:01 -0700

    Added undo button.  Moved x/y coord array definition out of conditional to allow use outside of conditional's scope.

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,12 @@ stickerCanvas.width = 256;
 stickerCanvas.height = 256;
 document.body.appendChild(stickerCanvas);
 const pen = stickerCanvas.getContext("2d");
+const xCoords: number[] = [];
+const yCoords: number[] = [];
 
 if (pen){
     let drawing: boolean = false;
-    const xCoords: number[] = [];
-    const yCoords: number[] = [];
-    
+
     stickerCanvas.addEventListener("mousedown", (e) => {
         drawing = true;
         xCoords.push(e.offsetX);
@@ -58,6 +58,20 @@ document.body.appendChild(clearButton);
 clearButton.addEventListener("click", () => {
     if (pen) {
         pen.clearRect(0, 0, stickerCanvas.width, stickerCanvas.height);
+    }
+    else {
+        console.error("Could not get 2D context from canvas");
+    }
+});
+
+const undoButton = document.createElement("button");
+undoButton.textContent = "Undo";
+document.body.appendChild(undoButton);
+undoButton.addEventListener("click", () => {
+    if (pen) {
+        xCoords.pop();
+        yCoords.pop();
+        stickerCanvas.dispatchEvent(new Event("drawing-changed"));
     }
     else {
         console.error("Could not get 2D context from canvas");
