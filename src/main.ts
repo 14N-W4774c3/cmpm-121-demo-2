ee7d28a1d1849ef361ffb815f7cb72f7b1fd5779
14N-W4774c3 <iawallac@ucsc.edu>
2024-10-24 21:08:04 -0700

    Implemented line objects, and revised drawing, undo and redo functionality to use lines instead of X/Y arrays.

--- src/main.ts
+++ src/main.ts
@@ -14,24 +14,70 @@ stickerCanvas.width = 256;
 stickerCanvas.height = 256;
 document.body.appendChild(stickerCanvas);
 const pen = stickerCanvas.getContext("2d");
+/*
 const xCoords: number[] = [];
 const yCoords: number[] = [];
 const redoXCoords: number[] = [];
 const redoYCoords: number[] = [];
+const line = {
+    xCoords: number[], 
+    yCoords: number[], 
+    function line(xCoord: number, yCoord: number){
+        this.xCoords.push(xCoord);
+        this.yCoords.push(yCoord);
+    },
+    display(ctx: CanvasRenderingContext2D){
+        ctx.beginPath();
+        ctx.moveTo(this.xCoords[0], this.yCoords[0]);
+        for (let i = 1; i < this.xCoords.length; i++) {
+            ctx.lineTo(this.xCoords[i], this.yCoords[i]);
+        }
+        ctx.stroke();
+    }
+};
+*/
+
+type line = {
+    xCoords: number[], 
+    yCoords: number[], 
+    display: (ctx: CanvasRenderingContext2D) => void
+};
+function createLine(xCoord: number, yCoord: number): line{
+    return {
+        xCoords: [xCoord],
+        yCoords: [yCoord],
+        display(ctx: CanvasRenderingContext2D){
+            ctx.beginPath();
+            ctx.moveTo(this.xCoords[0], this.yCoords[0]);
+            for (let i = 1; i < this.xCoords.length; i++) {
+                ctx.lineTo(this.xCoords[i], this.yCoords[i]);
+            }
+            ctx.stroke();
+        }
+    };
+}
+const displayedLines: line[] = [];
+const redoStack: line[] = [];
 
 if (pen){
     let drawing: boolean = false;
-
     stickerCanvas.addEventListener("mousedown", (e) => {
         drawing = true;
+        /*
         xCoords.push(e.offsetX);
         yCoords.push(e.offsetY);
+        */
+        displayedLines.push(createLine(e.offsetX, e.offsetY));
     });
 
     stickerCanvas.addEventListener("mousemove", (e) => {
         if (drawing) {
+            /*
             xCoords.push(e.offsetX);
             yCoords.push(e.offsetY);
+            */
+            displayedLines[displayedLines.length - 1].xCoords.push(e.offsetX);
+            displayedLines[displayedLines.length - 1].yCoords.push(e.offsetY);           
             stickerCanvas.dispatchEvent(new Event("drawing-changed"));
             }
     });
@@ -42,12 +88,15 @@ if (pen){
     
     stickerCanvas.addEventListener("drawing-changed", () => {
         pen.clearRect(0, 0, stickerCanvas.width, stickerCanvas.height);
+        /*
         pen.beginPath();
         pen.moveTo(xCoords[0], yCoords[0]);
         for (let i = 1; i < xCoords.length; i++) {
             pen.lineTo(xCoords[i], yCoords[i]);
         }
         pen.stroke();
+        */
+        displayedLines.forEach(line => line.display(pen));
     });
 }
 else {
@@ -70,9 +119,15 @@ const undoButton = document.createElement("button");
 undoButton.textContent = "Undo";
 document.body.appendChild(undoButton);
 undoButton.addEventListener("click", () => {
+    /*
     if (pen && xCoords.length > 0 && yCoords.length > 0) {
         redoXCoords.push(xCoords.pop()!);
         redoYCoords.push(yCoords.pop()!);
+        stickerCanvas.dispatchEvent(new Event("drawing-changed"));
+    }
+    */
+    if (pen && displayedLines.length > 0) {
+        redoStack.push(displayedLines.pop()!);
         stickerCanvas.dispatchEvent(new Event("drawing-changed"));
     }
     else {
@@ -84,9 +139,15 @@ const redoButton = document.createElement("button");
 redoButton.textContent = "Redo";
 document.body.appendChild(redoButton);
 redoButton.addEventListener("click", () => {
+    /*
     if (pen && redoXCoords.length > 0 && redoYCoords.length > 0) {
         xCoords.push(redoXCoords.pop()!);
         yCoords.push(redoYCoords.pop()!);
+        stickerCanvas.dispatchEvent(new Event("drawing-changed"));
+    }
+    */
+    if (pen && redoStack.length > 0) {
+        displayedLines.push(redoStack.pop()!);
         stickerCanvas.dispatchEvent(new Event("drawing-changed"));
     }
     else {
